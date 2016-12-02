28902a8e5a904630aaf0976353b92c51fdcf88a8
Macy Medema <macymedema@gmail.com>
2016-12-02 13:49:38 -0800

    fixing login

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,15 +1,15 @@
-function formhash(form, key) {
+function formhash(form, password) {
     // Create a new element input, this will be our hashed password field.
-    var k = document.createElement("input");
+    var p = document.createElement("input");
 
     // Add the new element to our form.
-    form.appendChild(k);
-    k.name = "k";
-    k.type = "hidden";
-    k.value = hex_sha512(key.value);
+    form.appendChild(p);
+    p.name = "p";
+    p.type = "hidden";
+    p.value = hex_sha512(password.value);
 
     // Make sure the plaintext password doesn't get sent.
-    key.value = "";
+    password.value = "";
 
     // Finally submit the form.
     form.submit();
@@ -17,7 +17,7 @@ function formhash(form, key) {
 
 function regformhash(form, voterID, voterKey, conf) {
      // Check each field has a value
-    if (  voterID.value == ''     ||
+    if (voterID.value == ''         ||
           voterKey.value == ''  ||
           conf.value == '') {
 
@@ -25,32 +25,6 @@ function regformhash(form, voterID, voterKey, conf) {
         return false;
     }
 
-    // Check the username
-
-    re = /^\w+$/;
-    if(!re.test(form.voterID.value)) {
-        alert("VoterID must contain only letters, numbers and underscores. Please try again");
-        form.voterID.focus();
-        return false;
-    }
-
-    // Check that the password is sufficiently long (min 6 chars)
-    // The check is duplicated below, but this is included to give more
-    // specific guidance to the user
-    if (voterKey.value.length < 6) {
-        alert('Passwords must be at least 6 characters long.  Please try again');
-        form.voterKey.focus();
-        return false;
-    }
-
-    // At least one number, one lowercase and one uppercase letter
-    // At least six characters
-
-    //var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    //if (!re.test(password.value)) {
-    //    alert('Passwords must contain at least one number, one lowercase and one uppercase letter.  Please try again');
-    //    return false;
-    //}
 
     // Check password and confirmation are the same
     if (voterKey.value != conf.value) {
@@ -60,15 +34,15 @@ function regformhash(form, voterID, voterKey, conf) {
     }
 
     // Create a new element input, this will be our hashed password field.
-    var k = document.createElement("input");
+    var p = document.createElement("input");
 
     // Add the new element to our form.
-    form.appendChild(k);
-    k.name = "k";
-    k.type = "hidden";
-    k.value = hex_sha512(voterKey.value);
+    form.appendChild(p);
+    p.name = "p";
+    p.type = "hidden";
+    p.value = hex_sha512(voterKey.value);
 
-    // Make sure the plaintext password doesn't get sent. 
+    // Make sure the plaintext password doesn't get sent.
     voterKey.value = "";
     conf.value = "";
 
